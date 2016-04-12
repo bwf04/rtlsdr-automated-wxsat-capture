170815f65f14dbbf7341684f03c37e191054ecb5
cyber-atomus <atomus@atomus.eu.org>
2016-04-12 11:14:00 +0200

    Updated, should work IF predict is in /usr/bin/. Otherwise please change accordingly.

--- update-keps.sh
+++ update-keps.sh
@@ -8,4 +8,10 @@ wget -qr https://www.celestrak.com/NORAD/elements/noaa.txt -O /tmp/noaa.txt
 rm /tmp/amateur.txt
 wget -qr https://www.celestrak.com/NORAD/elements/amateur.txt -O /tmp/amateur.txt
 
-/usr/bin/predict -u /tmp/noaa.txt /tmp/amateur.txt
+rm /tmp/cubesat.txt
+wget -qr https://www.celestrak.com/NORAD/elements/cubesat.txt -O /tmp/cubesat.txt
+
+rm /tmp/weather.txt
+wget -qr https://www.celestrak.com/NORAD/elements/weather.txt -O /tmp/weather.txt
+
+/usr/bin/predict -u /tmp/noaa.txt /tmp/amateur.txt /tmp/weather.txt /tmp/cubesat.txt
